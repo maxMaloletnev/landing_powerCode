b49ad96abaeff6704c5380bdb9add50fb09c28bd
maxMaloletnev <maksym.malolietniev@gmail.com>
2019-06-04 22:03:04 +0300

    work

---
+++ 123/js/script.js
@@ -0,0 +1,88 @@
+//var n = prompt("Qnt apples");
+////var text;
+////
+////if(n==1){
+////    text = ' Apple'
+////}else {
+////    text = ' Apples'
+////}
+//var text = n==1 ? " Apple" : " Apples"
+//
+//console.log(n + text)
+
+//var m = +prompt ('Введите 1 число');
+//
+//if (m<0){
+//    console.log ("Число меньше 0")
+//} else if (m>=0 && m<100) {
+//    console.log ("Число от 0 до 100")
+//} else if (m==100) {
+//    console.log ("Число равно 100")
+//} else {
+//    console.log ("Число больше 100")
+//}
+
+
+
+
+
+//var name = prompt ('Введите имя');
+//var surname = prompt ('Введите фамилию');
+//var home = prompt ('Команда хозяева');
+//var guest = prompt ('Команда гости')
+//var stadium = prompt ('Стадион')
+//var sector = prompt ('Сектор')
+//var row = prompt ('Ряд')
+//var place = prompt ('Место')
+//var time = prompt ('Время')
+//
+//var ticket = 'Имя Фамилия: ';
+//ticket += name + ' ' + surname;
+//ticket += '; Команда хозяева: ' + home
+//ticket += '; Команда гости: '+ guest
+//ticket += '; Стадион: '+ stadium
+//ticket += '; Сектор: '+ sector
+//ticket += '; Ряд: '+ row
+//ticket += '; Место: '+ place
+//ticket += '; Время: '+ time
+//
+//alert (ticket)
+//console.log (ticket)
+//////
+//////
+//////
+//////var m = +prompt ('Введите 1 число');
+//////var n = +prompt ('Введите 2 число');
+//////alert (m*n)
+////var n = 5;
+////var str = 'string';
+////var bool = true;
+////var ggg = null;
+//////console.log(str);
+//////console.log(ggg);
+////var group = {name:'Быки', teacher:1, students:6,}
+////
+////var message ="Назварние: ";
+////message += group.name;
+////message += "; Учитель "
+////message += group.teacher;
+////message += "; Студенты "
+////message += group.students;
+////console.log (message);
+////var year = prompt ("В каком году Вы пошли на курсы")
+////if (year =='2019') {
+////    alert('Верно');
+////} else {
+////    alert("Пробуем еще")
+////}
+//
+//var m = +prompt ('Введите 1 число');
+//var n = +prompt ('Введите 2 число');
+//if (m > n) {
+//    console.log(m + ' > ' + n);
+//} else if (n==m){
+//    console.log(m + ' = ' + n);
+//} else {
+//    console.log(m + ' < ' + n);
+//}
+//
